7869200febd697072a3a245d8cb06aa7c9430086
Danijel-James Wynyard <danijel.james@me.com>
2017-08-02 15:38:28 +0930

    create DriveSelected.ps1

---
+++ script-tools/DriveSelected.ps1
@@ -0,0 +1,9 @@
+﻿$global:DriveSelected = $NULL
+
+function global:WhichDrive2BackupTo{
+    Write-Host "From the drives listed,"
+    Write-Host "select the drive to backup"
+    $global:DriveSelected = Read-Host "your data to:"
+    Write-Host ""
+    Write-Host "Drive selected is" $global:DriveSelected
+    }
