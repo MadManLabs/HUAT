1757647cb012c3227cd59954359bacef4465e03e
Danijel-James Wynyard <danijel.james@me.com>
2017-08-02 15:45:23 +0930

    add variable dependency check

--- script-tools/DriveSelected.ps1
+++ script-tools/DriveSelected.ps1
@@ -1,6 +1,13 @@
 ﻿$global:DriveSelected = $NULL
 
 function global:WhichDrive2BackupTo{
+    If (Get-Variable $DriveSelected -Scope Global -ErrorAction SilentlyContinue){
+    }Else{
+        Write-Host "DriveSelected variable not defined!"
+        Write-Host "Exiting in 3 seconds"
+        Start-Sleep -s 3
+        Exit
+    }
     Write-Host "From the drives listed,"
     Write-Host "select the drive to backup"
     $global:DriveSelected = Read-Host "your data to:"
