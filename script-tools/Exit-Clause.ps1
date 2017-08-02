7c37bc437608a49aefb68cea384bab5e2a04b597
Danijel-James Wynyard <danijel.james@me.com>
2017-08-02 11:20:05 +0930

    Exit Clause sciptlet

---
+++ script-tools/Exit-Clause.ps1
@@ -0,0 +1,9 @@
+﻿function global:ExitClause{
+    Clear-Host
+    Write-Host ""
+    Write-Host "Invalid entry received!" -ForegroundColor Red -BackgroundColor Black
+    Write-Host ""
+    Write-Host "Exiting script!" -ForegroundColor DarkYellow -BackgroundColor Black
+    Start-Sleep -s 5
+    exit
+}
