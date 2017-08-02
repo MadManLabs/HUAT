71cbf247c79116966ed6ae6daf2bbcde80063e87
Danijel-James Wynyard <danijel.james@me.com>
2017-08-02 12:56:19 +0930

    UserIDCorrectCheck

---
+++ script-tools/is-userid-correct.ps1
@@ -0,0 +1,13 @@
+﻿$global:IsUserIDCorrect = $NULL
+$global:BackupUserID = $NULL
+
+global function:UserIDCorrect{
+    If($global:IsUserIDCorrect = $NULL){
+        Write-Host "UserID is not set"
+    }
+    ElseIf($global:IsUserIDCorrect = 1){
+        $global:BackupUserID = $env:USERNAME
+    }Else{
+        $global:BackupUserID = Write-Host "Enter UserID to backup"
+    }
+}
