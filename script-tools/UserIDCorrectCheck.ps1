9219a93457e166736dc37af9c0a0da4e1c9d0d1c
Danijel-James Wynyard <danijel.james@me.com>
2017-08-02 13:07:30 +0930

    upgraded script with beeps

--- script-tools/UserIDCorrectCheck.ps1
+++ script-tools/UserIDCorrectCheck.ps1
@@ -1,11 +1,19 @@
 ﻿$global:IsUserIDCorrect = $NULL
 $global:BackupUserID = $NULL
 
-global function:UserIDCorrect{
-    If($global:IsUserIDCorrect = $NULL){
-        Write-Host "UserID is not set"
+function global:UserIDCorrect{
+    If($global:IsUserIDCorrect -eq $NULL){
+        [console]::beep(2000,500)
+        [console]::beep(2000,500)
+        [console]::beep(2000,500)
+        Write-Host "UserID is not set!" -ForegroundColor Red -BackgroundColor Black
+        Start-Sleep 2
+        Write-Host "Exiting!" -ForegroundColor Yellow -BackgroundColor Black
+        [console]::beep(2000,500)
+        Start-Sleep 3
+        Exit
     }
-    ElseIf($global:IsUserIDCorrect = 1){
+    ElseIf($global:IsUserIDCorrect -eq 1){
         $global:BackupUserID = $env:USERNAME
     }Else{
         $global:BackupUserID = Write-Host "Enter UserID to backup"
