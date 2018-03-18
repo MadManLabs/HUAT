d7afa0c1633c0fdff0d92e6a8445535f8062a8a0
Danijel-James Wynyard <danijel.james@me.com>
2018-03-18 18:37:45 +1100

    defunct file

--- inv/scripts/try-catch.ps1
+++
@@ -1,8 +0,0 @@
-try {
-        $colItems = Get-ChildItem $folder.FullName -Recurse -ErrorAction STOP | Measure-Object -property length -sum -ErrorAction SilentlyContinue
-        $size = "{0:N2}" -f ($colItems.sum / 1MB) + " MB"
-    } catch {
-        Write-Warning "Access Denied to $folder.Name"
-        $size = "N/A"
-        Continue
-    }
