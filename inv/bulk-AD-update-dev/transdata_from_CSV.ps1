15b98cffcfd54db95921761014aec4e45bebf558
Danijel-James Wynyard <danijel.james@me.com>
2018-03-18 22:34:40 +1100

    read data from CSV then update specified fields

---
+++ inv/bulk-AD-update-dev/transdata_from_CSV.ps1
@@ -0,0 +1,7 @@
+﻿Import-Module ActiveDirectory
+$Userscsv = Import-Csv ;'.\allUsers2.csv'
+
+ForEach ($User in $Userscsv)
+{
+    Set-ADUser $User.sAMAccountName -Replace @{Division = $User.Division; Office = $User.Office; City = $User.City}
+}
