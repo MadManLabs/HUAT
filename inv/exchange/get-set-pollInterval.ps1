c1d9287b1ef584cdbb035a0f3781a2a9fb94a6f9
Danijel-James Wynyard <danijel.james@me.com>
2018-03-19 15:17:32 +1100

    exchange files

---
+++ inv/exchange/get-set-pollInterval.ps1
@@ -0,0 +1,7 @@
+﻿## Get poll interval
+
+Get-OabVirtualDirectory | select server,name,pollinterval
+
+## Set poll interval
+
+Get-OabVirtualDirectory | Set-OabVirtualDirectory -PollInterval 120
---
+++ inv/exchange/toConnect.ps1
@@ -0,0 +1,9 @@
+﻿$UserCredential = Get-Credential
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://pinvxc101/PowerShell/ -Authentication Kerberos -Credential $UserCredential
+
+<#
+To remove session, type:
+
+    Remove-PSSession $Session
+
+#>
