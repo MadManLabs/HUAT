befb2d95eca692e7e8958f59d38da4c69b013466
Danijel-James Wynyard <danijel.james@me.com>
2018-03-19 13:17:00 +1100

    server online or not, merge

---
+++ inv/server-online-or-not/firstServerDeploy_check_servers_online_report.ps1
@@ -0,0 +1,54 @@
+﻿$firstServerDeploy = @('DINVAPI01',
+    'DINVPC11',
+    'DINVPC12',
+    'SINCIDTT01',
+    'SINVAPI01',
+    'SINVCMS01',
+    'TINVMAP01',
+    'TINVSQL01',
+    'TINVUTIL02',
+    'TINVWEB02',
+    'TINVWEB03',
+    'CINVCTX01',
+    'CINVEBO08',
+    'CINVEFO08',
+    'DINVEBO08',
+    'DINVEFO08',
+    'TINVCOG10',
+    'TINVCTX11',
+    'TINVCTX12',
+    'TINVEBO08',
+    'TINVEFO08',
+    'TINVEWE01',
+    'TINVLMS02',
+    'TINVMAP02',
+    'TINVSPDB10',
+    'TINVSPS10',
+    'TINVSQLB01',
+    'TINVWEB04',
+    'DINVCOGTM1',
+    'ERPDEMO',
+    'DINVSF02',
+    'SINVSF02',
+    'DINVPC01',
+    'DINVPC02',
+    'DINVPC03',
+    'DINVPC04'
+)
+
+$sorted_firstServerDeploy = ($firstServerDeploy | Sort-Object)
+
+$logPath = 'C:\scripts\logs\'
+$logName = 'firstServerDeployLog.txt'
+
+$sorted_firstServerDeploy | ForEach-Object -Process `
+{
+    If (!(Test-Connection -Cn $_ -BufferSize 16 -Count 1 -ea 0 -Quiet))
+    {
+        "$_ not online" | Out-File -Encoding ascii -Append $logPath$logName
+    }
+    Else
+    {
+        Get-WmiObject -Class Win32_Processor -ComputerName $_ | Select-Object AddressWidth
+    }
+}    
---
+++ inv/server-online-or-not/secondServerDeploy_check_servers_online_report.ps1
@@ -0,0 +1,95 @@
+﻿$secondServerDeploy = @('PINVAPI01',
+    'PINVERSGW01',
+    'PINVHUB01',
+    'PINVRSOLID01',
+    'PINVADC01',
+    'PINVCMS01',
+    'PINVIDT01',
+    'PINVHPSIM01',
+    'PINVEBR01',
+    'PINVEBRPX01',
+    'PINVEMC01',
+    'PINVSF03',
+    'PINVCHQ01',
+    'PINVCOGSPSS02',
+    'PINVFRX01',
+    'PINVMAP01',
+    'PINVMGT01',
+    'PINVCTX43',
+    'PINVCTX45',
+    'PINVCTX46',
+    'PINVCTX47',
+    'PINVCTX49',
+    'PINVCTX52',
+    'PINVCTX53',
+    'PINVCTX54',
+    'PINVCTX55',
+    'PINVCTX56',
+    'PINVERM02',
+    'PINVEVDA01',
+    'PINVEVJR01',
+    'PINVFLW08',
+    'PINVKVS02',
+    'PINVMDM01',
+    'PINVMDM02',
+    'PINVMGT02',
+    'PINVREP08',
+    'PINVUTIL02',
+    'PINVUTIL03',
+    'PINVXMP02',
+    'PINVMGTSQL01',
+    'PINVOSCE01',
+    'PINVPRINT01',
+    'PINVUTIL04',
+    'PINVUTIL05',
+    'PINVUTIL06',
+    'PINVMEDC01',
+    'PINVWSUS02',
+    'PINVCAG02',
+    'PINVCAG03',
+    'PINVGW02',
+    'WNBull-FMS',
+    'PINVESX01-09',
+    'PINVDOC01',
+    'PINVCOGSPSS01',
+    'PINVCOG01',
+    'PINVSQL01',
+    'PINVWEB01',
+    'PINVWEB02',
+    'PINVWEB05',
+    'PINVAD801',
+    'PINVAD802',
+    'PINVCOGAP01',
+    'PINVCOGCM01',
+    'PINVCOGDC01',
+    'PINVEBO08',
+    'PINVEFO08',
+    'PINVFP02',
+    'PINVFP03',
+    'PINVLIC01',
+    'PINVLMS02',
+    'PINVSF01',
+    'PINVSPDB10',
+    'PINVSPS10',
+    'PINVSQLB01',
+    'PINVPCUBE02',
+    'PINVCOG02'
+)
+
+$sorted_secondServerDeploy = ($secondServerDeploy | Sort-Object)
+
+$logPath = 'C:\scripts\logs\'
+$logName = 'secondServerDeployLog.txt'
+$successLogName = 'successLog2.txt'
+
+$sorted_firstServerDeploy | ForEach-Object -Process `
+{
+    If (!(Test-Connection -Cn $_ -BufferSize 16 -Count 1 -ea 0 -Quiet))
+    {
+        "$_ not online" | Out-File -Encoding ascii -Append $logPath$logName
+    }
+    Else
+    {
+        Write-Host "$_ $(Get-WmiObject -Class Win32_Processor -ComputerName $_ | Select-Object AddressWidth)"## | Out-File -Encoding ascii -Append $logPath$successLogName
+    }
+}    
